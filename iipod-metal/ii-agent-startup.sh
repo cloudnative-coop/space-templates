e45695dd2df3cb2b74a84eb928322e361e73c222
Hippie Hacker <hh@ii.coop>
2023-06-12 00:04:48 +0300

    Weekend at Bernies

--- iipod-metal/ii-agent-startup.sh
+++ iipod-metal/ii-agent-startup.sh
@@ -23,3 +23,29 @@ cd ~/go/src/k8s.io
 git clone https://github.com/kubernetes/kubernetes.git
 cd kubernetes
 git remote add ii git@github.com:ii/kubernetes.git
+# sudo apt-get install -y novnc websockify tigervnc-standalone-server icewm kitty
+# mkdir novnc && ln -s /usr/share/novnc/* novnc
+# cp novnc/vnc.html novnc/index.html
+# websockify -D --web=/home/ii/novnc 6080 localhost:5901
+# tigervncserver -useold -desktop $SESSION_NAME -SecurityTypes None#
+# export DISPLAY=:1
+# kitty -T "${lower(data.coder_workspace.ii.name)}" --detach --hold bash -c "cd minecraftforge && ./gradlew runClient"
+# Setup Istio
+echo "Install istio into this cluster..."
+helm repo add istio https://istio-release.storage.googleapis.com/charts
+helm repo update
+kubectl get ns istio-system || kubectl create ns istio-system
+helm install istio-base istio/base -n istio-system
+kubectl api-resources | grep istio
+helm ls -n istio-system
+helm install istiod istio/istiod -n istio-system --wait
+helm ls -n istio-system
+helm status istiod -n istio-system
+kubectl get deployments -n istio-system --output wide
+# Setup Knative
+echo "Install knative into this cluster..."
+kubectl apply -f https://github.com/knative/serving/releases/download/knative-v1.10.2/serving-crds.yaml
+kubectl apply -f https://github.com/knative/serving/releases/download/knative-v1.10.2/serving-core.yaml
+kubectl apply -f https://github.com/knative/net-istio/releases/download/knative-v1.10.1/net-istio.yaml
+kubectl --namespace istio-system get service istio-ingressgateway
+kubectl get pods -n knative-serving
--- iipod-metal/workspace.tf
+++ iipod-metal/workspace.tf
@@ -11,13 +11,25 @@ resource "coder_metadata" "device_info" {
   count       = data.coder_workspace.ii.transition == "start" ? 1 : 0
   resource_id = equinix_metal_device.machine.id
   item {
+    key   = "ssh"
+    value = "ssh -tA ii@${local.dns_fqdn} tmux at"
+  }
+  item {
+    key   = "ssh pod"
+    value = "ssh -t ii@${local.dns_fqdn} kubectl exec -it iipod-0 -- tmux at"
+  }
+  item {
+    key   = "scp kubeconfig"
+    value = "ssh root@${equinix_metal_device.machine.access_public_ipv4}:.kube/config /tmp/kubeconfig ; export KUBECONFIG=/tmp/kubeconfig"
+  }
+  item {
     key   = "direct ssh"
     value = "ssh root@${equinix_metal_device.machine.access_public_ipv4}"
   }
-  item {
-    key   = "serial console"
-    value = "ssh ${equinix_metal_device.machine.id}@sos.${equinix_metal_device.machine.deployed_facility}.platformequinix.com"
-  }
+  # item {
+  #   key   = "serial console"
+  #   value = "ssh ${equinix_metal_device.machine.id}@sos.${equinix_metal_device.machine.deployed_facility}.platformequinix.com"
+  # }
   item {
     key   = "equinix console"
     value = "https://console.equinix.com/devices/${equinix_metal_device.machine.id}/overview"

0c65be7aa4eb5147d3b9903fcafc96bc78dfc0c7
Hippie Hacker <hh@ii.coop>
2023-06-12 11:17:03 +0300

    Use metros c3.small.x86 normal availability
    Co-Authored-By: Stephen Heywood <stephen@ii.coop>

--- iipod-metal/parameters.tf
+++ iipod-metal/parameters.tf
@@ -5,6 +5,16 @@ variable "project" {
   default     = "f4a7273d-b1fc-4c50-93e8-7fed753c86ff" # pair.sharing.io
 }
 
+
+# https://deploy.equinix.com/developers/docs/metal/locations/capacity/#checking-capacity
+# We may want to check capacity now and then
+# metal capacity get -P c3.small.x86 | grep normal
+# | ny5  | c3.small.x86 | normal      |
+# | ny7  | c3.small.x86 | normal      |
+# | sv15 | c3.small.x86 | normal      |
+# | sv16 | c3.small.x86 | normal      |
+# | ty11 | c3.small.x86 | normal      |
+# Then updateplans
 data "coder_parameter" "metro" {
   name         = "metro"
   display_name = "Metro"
@@ -12,25 +22,30 @@ data "coder_parameter" "metro" {
   default      = "dc"
   icon         = "https://raw.githubusercontent.com/matifali/logos/main/docker.svg"
   option {
-    name  = "Sydney, Australia"
-    value = "sy"
-    icon  = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Flag_of_the_United_States_%28DoS_ECA_Color_Standard%29.svg/1200px-Flag_of_the_United_States_%28DoS_ECA_Color_Standard%29.svg.png"
-  }
-  option {
     name  = "Dallas, Texas, USA"
-    value = "da"
+    value = "ny"
     icon  = "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f7/Flag_of_Texas.svg/1200px-Flag_of_Texas.svg.png"
-  }
-  option {
-    name  = "Washington, DC, USA"
-    value = "dc"
-    icon  = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Flag_of_the_United_States_%28DoS_ECA_Color_Standard%29.svg/1200px-Flag_of_the_United_States_%28DoS_ECA_Color_Standard%29.svg.png"
   }
   option {
     name  = "Silicon Valley, USA"
     value = "sv"
     icon  = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Flag_of_the_United_States_%28DoS_ECA_Color_Standard%29.svg/1200px-Flag_of_the_United_States_%28DoS_ECA_Color_Standard%29.svg.png"
   }
+  option {
+    name  = "Tokyo, Japan"
+    value = "ty"
+    icon  = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Flag_of_the_United_States_%28DoS_ECA_Color_Standard%29.svg/1200px-Flag_of_the_United_States_%28DoS_ECA_Color_Standard%29.svg.png"
+  }
+  # option {
+  #   name  = "Sydney, Australia"
+  #   value = "sy"
+  #   icon  = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Flag_of_the_United_States_%28DoS_ECA_Color_Standard%29.svg/1200px-Flag_of_the_United_States_%28DoS_ECA_Color_Standard%29.svg.png"
+  # }
+  # option {
+  #   name  = "Washington, DC, USA"
+  #   value = "dc"
+  #   icon  = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Flag_of_the_United_States_%28DoS_ECA_Color_Standard%29.svg/1200px-Flag_of_the_United_States_%28DoS_ECA_Color_Standard%29.svg.png"
+  # }
 }
 
 data "coder_parameter" "plan" {

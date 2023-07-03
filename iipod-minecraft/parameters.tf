3457d1c1476a379629a65641476ff9d92422882c
Hippie Hacker <hh@ii.coop>
2023-07-04 01:40:40 +0300

    Bump iipod-minecraft image tag

--- iipod-minecraft/parameters.tf
+++ iipod-minecraft/parameters.tf
@@ -2,6 +2,6 @@ data "coder_parameter" "container-image" {
   name         = "container-image"
   display_name = "Container Image"
   description  = "The container image to use for the workspace"
-  default      = "ghcr.io/cloudnative-coop/iipod-minecraft:2023.07.04-1"
+  default      = "ghcr.io/cloudnative-coop/iipod-minecraft:2023.07.04-2"
   icon         = "https://raw.githubusercontent.com/matifali/logos/main/docker.svg"
 }

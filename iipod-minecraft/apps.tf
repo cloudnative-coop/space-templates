e9713181f394a6467277cd207152a6080c87b7ec
Hippie Hacker <hh@ii.coop>
2023-07-04 00:51:06 +0300

    Update coder_apps for iipod-minecraft
    Co-Authored-By: Abby Crimlis <abbymcrilis@yahoo.com>

--- iipod-minecraft/apps.tf
+++ iipod-minecraft/apps.tf
@@ -1,13 +1,13 @@
-# emacs
-resource "coder_app" "Emacs" {
-  subdomain    = true
-  share        = "public"
-  agent_id     = coder_agent.iipod.id
-  slug         = "emacs"
-  display_name = "Emacs"
-  icon         = "https://upload.wikimedia.org/wikipedia/commons/0/08/EmacsIcon.svg" # let's maybe get an emacs.svg somehow
-  url          = "http://localhost:8085"                                             # port 8080 + BROADWAY_DISPLAY
-}
+# # emacs
+# resource "coder_app" "Emacs" {
+#   subdomain    = true
+#   share        = "public"
+#   agent_id     = coder_agent.iipod.id
+#   slug         = "emacs"
+#   display_name = "Emacs"
+#   icon         = "https://upload.wikimedia.org/wikipedia/commons/0/08/EmacsIcon.svg" # let's maybe get an emacs.svg somehow
+#   url          = "http://localhost:8085"                                             # port 8080 + BROADWAY_DISPLAY
+# }
 
 # ttyd
 resource "coder_app" "tmux" {
@@ -20,16 +20,27 @@ resource "coder_app" "tmux" {
   url          = "http://localhost:7681" # 7681 is the default ttyd port
 }
 
-# web
-resource "coder_app" "web" {
+# vnc
+resource "coder_app" "vnc" {
   subdomain    = true
   share        = "public"
-  slug         = "web"
-  display_name = "web"
-  # icon         = "https://cdn.icon-icons.com/icons2/2148/PNG/512/tmux_icon_131831.png"
-  agent_id = coder_agent.iipod.id
-  url      = "http://localhost:8000" # 7681 is the default ttyd port
+  slug         = "vnc"
+  display_name = "vnc"
+  icon         = "/icon/novnc.svg"
+  agent_id     = coder_agent.iipod.id
+  url          = "http://localhost:6080?autoconnect=true&resize=remote" # 7681 is the default ttyd port
 }
+
+# # web
+# resource "coder_app" "web" {
+#   subdomain    = true
+#   share        = "public"
+#   slug         = "web"
+#   display_name = "web"
+#   # icon         = "https://cdn.icon-icons.com/icons2/2148/PNG/512/tmux_icon_131831.png"
+#   agent_id = coder_agent.iipod.id
+#   url      = "http://localhost:8000" # 7681 is the default ttyd port
+# }
 
 # resource "coder_app" "code-server" {
 #   agent_id     = coder_agent.ii.id

68d0d6752dc12eb9188810a007941693280b9f1d
Hippie Hacker <hh@ii.coop>
2023-07-04 00:56:03 +0300

    Not redirecting to domain for now

--- iipod-minecraft/iipod-startup.sh
+++ iipod-minecraft/iipod-startup.sh
@@ -6,62 +6,7 @@ echo "Starting TTYD"
 ttyd tmux at 2>&1 | tee /tmp/ttyd.log &
 
 cp -a /usr/share/novnc ~/novnc
-# mkdir ~/novnc && ln -s /usr/share/novnc/* ~/novnc
-cat <<EOF >~/novnc/index.html
-<!DOCTYPE html>
-<html>
-  <head>
-<meta
-  http-equiv="Refresh"
-  content="0; url='https://vnc.$SPACE_DOMAIN/vnc.html?autoconnect=true&resize=remote'" />
-<style>
-#loading-circle {
-  border: 4px solid #f3f3f3;
-  border-top: 4px solid rgb(4, 255, 4);
-  border-radius: 50%;
-  width: 40px;
-  height: 40px;
-  animation: spin 1s linear infinite;
-}
-body {
-    background: linear-gradient(to bottom, yellow, blue);
-    margin: 0;
-    padding: 0;
-    height:100vh;
-    font-size: 40px;
-    font-family: Arial, Helvetica, Calibri, sans-serif;
-    color:white;
-}
-.centered {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%,-50%);
-    display:flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-}
-.ml-50{
-    margin-left: 50px;
-}
-@keyframes spin {
-  0% { transform: rotate(0deg); }
-  100% { transform: rotate(360deg); }
-}
-
-</style>
-<body>
-    <div class="centered">
-        <p>You will be redirected to <a href="https://vnc.$SPACE_DOMAIN/vnc.html?autoconnect=true&resize=remote">VNC</a> soon!</p>
-        <div id="loading-circle"></div>
-    </div>
- </body>
-<script>
-  document.getElementById("loading-circle").style.display = "block";
-</script>
-</html>
-EOF
+cp ~/novnc/vnc.html ~/index.html
 
 tmux new -d -s "vnc" -n "novnc"
 tmux send-keys -t "vnc" "websockify -D --web=/home/ii/novnc 6080 localhost:5901

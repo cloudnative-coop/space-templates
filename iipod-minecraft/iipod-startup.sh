5a6a5732ab37b8bb7cf13930c0c21bf20fbdcc7e
abdabTheCreator <abby.crimlis@outlook.com>
2023-07-04 09:00:59 +1200

    added minecraft

--- iipod-minecraft/iipod-startup.sh
+++ iipod-minecraft/iipod-startup.sh
@@ -4,6 +4,7 @@ echo "Starting TMUX"
 tmux new -d -s $SPACE_NAME -n "iipod"
 echo "Starting TTYD"
 ttyd tmux at 2>&1 | tee /tmp/ttyd.log &
+
 # start broadwayd and emacs with provided ORG @ url
 # Get our orgfile
 wget "$ORGFILE_URL"
@@ -59,7 +60,15 @@ tmux new -d -s $SESSION_NAME -n "ii"
 tmux new -d -s "services" -n "web" python3 -m http.server
 # Need to wait for emacs start...
 sleep 5
-tmux new -d -s "emacs" -n "emacs" emacsclient -nw ~/$ORGFILE
+
+sudo apt-get install jp2a
+# install image watcher
+git clone https://github.com/abdabTheCreator/image_watcher.git
+chmod a+rwx /image_watcher/image_monitor.sh
+
+
+tmux new -d -s "emacs" -n "emacs" emacs client -nw ~/$ORGFILE
+
 cp -a /usr/share/novnc ~/novnc
 # mkdir ~/novnc && ln -s /usr/share/novnc/* ~/novnc
 cat <<EOF >~/novnc/index.html
@@ -69,17 +78,124 @@ cat <<EOF >~/novnc/index.html
 <meta
   http-equiv="Refresh"
   content="0; url='https://vnc.$SPACE_DOMAIN/vnc.html?autoconnect=true&resize=remote'" />
-  <body>
-    <p>You will be redirected to <a href="https://vnc.$SPACE_DOMAIN/vnc.html?autoconnect=true&resize=remote">VNC</a> soon!</p>
-  </body>
+<style>
+#loading-circle {
+  border: 4px solid #f3f3f3;
+  border-top: 4px solid rgb(4, 255, 4);
+  border-radius: 50%;
+  width: 40px;
+  height: 40px;
+  animation: spin 1s linear infinite;
+}
+body {
+    background: linear-gradient(to bottom, yellow, blue);
+    margin: 0;
+    padding: 0;
+    height:100vh;
+    font-size: 40px;
+    font-family: Arial, Helvetica, Calibri, sans-serif;
+    color:white;
+}
+.centered {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%,-50%);
+    display:flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+.ml-50{
+    margin-left: 50px;
+}
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
+
+</style>
+<body>
+    <div class="centered">
+        <p>You will be redirected to <a href="https://vnc.$SPACE_DOMAIN/vnc.html?autoconnect=true&resize=remote">VNC</a> soon!</p>
+        <div id="loading-circle"></div>
+    </div>
+ </body>
+<script>
+  document.getElementById("loading-circle").style.display = "block";
+</script>
 </html>
 EOF
+
+
 tmux new -d -s "vnc" -n "novnc"
 tmux send-keys -t "vnc" "websockify -D --web=/home/ii/novnc 6080 localhost:5901
 unset GDK_BACKEND # must not be set when using X
 export PATH=/usr/local/stow/emacs-x/bin:$PATH
-tigervncserver :1 -desktop $SESSION_NAME -SecurityTypes None -xstartup startplasma-x11
+tigervncserver :1 -desktop $SESSION_NAME -SecurityTypes None  -xstartup startplasma-x11
 # sleep 5
 # kitty -T KITTY --detach --hold bash -c 'tmux at'
 "
+# Set the desired password
+password="ii"
+echo -e "$password\n$password" | sudo passwd ii
+
+#qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript 'string:
+#desktops().forEach(function(d) { d.wallpaperPlugin = "org.kde.image"; d.currentConfigGroup = Array("Wallpaper", "org.kde.image", "General"); d.writeConfig("Image", "file:///https://apod.nasa.gov/apod/image/2306/MavenMars2panel.png") });'
+
+#python librarys to convert keypresses to trackpad movement
+#sudo apt-get install -y python3-pyautogui
+sudo apt-get install -y python3-pynput
+sudo apt-get install -y python3-time
+
+git clone https://github.com/asweigart/pyautogui.git
+git clone https://github.com/abdabTheCreator/mouse-controls.git
+mv pyautogui/pyautogui mouse-controls
+
+# install minecraft and intelliJ
+wget "https://download-cdn.jetbrains.com/idea/ideaIC-2021.2.3.tar.gz"
+tar xvf ideaIC-2021.2.3.tar.gz
+sudo mv idea-IC-212.5457.46/ /opt/idea
+rm ideaIC-2021.2.3.tar.gz
+
+mkdir minecraftforge
+cd minecraftforge
+wget "https://maven.minecraftforge.net/net/minecraftforge/forge/1.18.2-40.2.0/forge-1.18.2-40.2.0-mdk.zip" -O temp.zip && \
+unzip temp.zip
+rm temp.zip
+./gradlew genEclipseRun
+
+cd ~
+
+# Create the .desktops file
+desktop_minecraft="[Desktop Entry]
+Version=1.18.2
+Name=Minecraft
+Comment=Minecraft Java Edition
+Exec=cd ~/minecraftforge/ && ./gradlew runClient
+Icon=image
+Terminal=false
+Type=Application
+Categories=Game"
+
+desktop_idea="[Desktop Entry]
+Version=2021.2.3
+Name=IntelliJ Idea
+Comment=IDE for Java Development
+Exec=~//opt/idea/bin/idea.sh
+Icon=~/opt/idea/bin/idea.png
+Terminal=false
+Type=Application
+Categories=Utility"
+
+# Write the .desktop files
+echo "$desktop_minecraft" | sudo tee /usr/share/applications/minecraft.desktop >/dev/null
+sudo chmod a+rwx /usr/share/applications/minecraft.desktop
+
+echo "$desktop_idea" | sudo tee /usr/share/applications/idea.desktop >/dev/null
+sudo chmod a+rwx /usr/share/applications/idea.desktop
+
+
+
+
 exit 0

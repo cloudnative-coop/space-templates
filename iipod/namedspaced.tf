9ef32169bb7dea625e44812e5c9840443f96c6f5
Hippie Hacker <hh@ii.coop>
2023-07-18 09:38:50 +0200

    Push namespaces forward

--- iipod/namedspaced.tf
+++ iipod/namedspaced.tf
@@ -1,14 +1,55 @@
+resource "template_dir" "persistent" {
+  source_dir      = "${path.module}/persistent_manifests"
+  destination_dir = "${path.cwd}/persistent_manifests"
+  vars = {
+    namespace   = local.namespace
+    user_domain = local.user_domain
+  }
+}
+
+resource "template_dir" "ephemeral" {
+  source_dir      = "${path.module}/ephemeral_manifests"
+  destination_dir = "${path.cwd}/ephemeral_manifests"
+  vars = {
+    namespace    = local.namespace
+    user_domain  = local.user_domain
+    space_name   = local.space_name
+    space_domain = local.space_domain
+  }
+}
+
 resource "null_resource" "namespace" {
-  # Pour man's GET IT DONE
+  # I want to apply a bunch of manifests at once
+  # I encouge you to find a faster way
   provisioner "local-exec" {
     command = <<COMMAND
-~/kubectl version --client || (
+set -x
+set -e
+./kubectl version --client || (
   echo installing kubectl:
-  curl -L https://dl.k8s.io/release/v1.27.3/bin/linux/amd64/kubectl -o ~/kubectl \
-  && chmod +x ~/kubectl
+  curl -L https://dl.k8s.io/release/v1.27.3/bin/linux/amd64/kubectl -o ./kubectl \
+  && chmod +x ./kubectl
 )
 COMMAND
   }
+  # We have manifests to create the namespace and persist a few things
+  provisioner "local-exec" {
+    # - the main *.user.DOMAIN cert
+    command = <<COMMAND
+set -x
+set -e
+./kubectl apply -f ./persistent_mannifests/
+COMMAND
+  }
+  # We have manifests to create the namespace
+  provisioner "local-exec" {
+    command = <<COMMAND
+set -x
+set -e
+./kubectl apply -f ./ephemeral_mannifests/
+COMMAND
+  }
+  # We could also deloy logstream-kube to show the pod coming up...
   #   provisioner "local-exec" {
   #     command = <<COMMAND
   # ~/helm version --short || (
@@ -19,9 +60,6 @@ COMMAND
   # )
   # COMMAND
   #   }
-  provisioner "local-exec" {
-    command = "~/kubectl create ns ${local.namespace} || true"
-  }
   # provisioner "local-exec" {
   #   command = "~/kubectl -n ${local.namespace} apply -f ${path.module}/manifests/admin-sa.yaml"
   # }
@@ -34,161 +72,10 @@ COMMAND
   #   coder-logstream-kube coder-logstream-kube
   # COMMAND
   #   }
-  provisioner "local-exec" {
-    command = <<COMMAND
-cat <<MANIFEST | ~/kubectl apply --namespace ${local.namespace} -f -
----
-apiVersion: v1
-kind: ServiceAccount
-metadata:
-  name: admin
-automountServiceAccountToken: true
----
-apiVersion: rbac.authorization.k8s.io/v1
-kind: Role
-metadata:
-  name: admin
-rules:
-  - apiGroups:
-      - ""
-    resources:
-      - "*"
-    verbs:
-      - "*"
-  - apiGroups:
-      - "*"
-    resources:
-      - "*"
-    verbs:
-      - "*"
----
-apiVersion: rbac.authorization.k8s.io/v1
-kind: RoleBinding
-metadata:
-  name: admin
-roleRef:
-  apiGroup: rbac.authorization.k8s.io
-  kind: Role
-  name: admin
-subjects:
-  - kind: ServiceAccount
-    name: admin
----
-apiVersion: cert-manager.io/v1
-kind: Certificate
-metadata:
-  name: wildcard
-spec:
-  secretName: wildcard-tls
-  dnsNames:
-    - "${local.user_domain}"
-    - "*.${local.user_domain}"
-  issuerRef:
-    name: letsencrypt
-    kind: ClusterIssuer
-    group: cert-manager.io
----
-apiVersion: cert-manager.io/v1
-kind: Certificate
-metadata:
-  name: ${local.spacename}-wildcard-tls
-  labels:
-    spacename: ${local.spacename}
-spec:
-  secretName: ${local.spacename}-wildcard-tls
-  dnsNames:
-    - "${local.space_domain}"
-    - "*.${local.space_domain}"
-  issuerRef:
-    name: letsencrypt
-    kind: ClusterIssuer
-    group: cert-manager.io
-MANIFEST
-COMMAND
-  }
-  # ---
-  # apiVersion: source.toolkit.fluxcd.io/v1
-  # kind: GitRepository
-  # metadata:
-  #   name: itscontained
-  #   namespace: flux-system
-  # spec:
-  #   interval: 24h
-  #   url: https://github.com/itscontained/charts
-  #   ref:
-  #     branch: canon
-  #   ignore: |-
-  # exclude all
-  #    /*
-  # include charts directory
-  #    !/itscontained/raw/
-  #   provisioner "local-exec" {
-  #     command = <<COMMAND
-  # cat <<MANIFEST | ~/kubectl apply --namespace ${local.namespace} -f -
-  # MANIFEST
-  # COMMAND
-  #   }
 }
 resource "coder_metadata" "namespace" {
   resource_id = null_resource.namespace.id
   count       = data.coder_workspace.ii.start_count
   icon        = "/icon/k8s.png"
-  hide        = true
+  # hide        = true
 }
-# apiVersion:
-# kind:
-# metadata:
-#   name: ${local.spacename}
-#   labels:
-#     spacename: ${local.spacename}
-# spec:
-#   interval: 30m
-#   releaseName: ${local.spacename}
-#   chart:
-#     spec:
-#       chart: itscontained/raw
-#       sourceRef:
-#         kind: GitRepository
-#         name: itscontained
-#         namespace: flux-system
-#   values:
-#     resources:
-#       %{for path in fileset(path.module, "manifests/templates/*yaml")~}
-#       - ${indent(2, templatefile(path, { spacename = local.spacename, space_domain = local.space_domain }))}
-#       %{endfor~}
-# }
-# resource "kubernetes_manifest" "test-configmap" {
-#   manifest = {
-#     "apiVersion" = "helm.toolkit.fluxcd.io/v2beta1"
-#     "kind"       = "HelmRelease"
-#     "metadata" = {
-#       "name"      = "test-config"
-#       "namespace" = "default"
-#     }
-#     "data" = {
-#       "foo" = "bar"
-#     }
-#   }
-# }
-# resource "helm_release" "iipod" {
-#   name       = "iipod-${local.spacename}"
-#   repository = "https://charts.itscontained.io"
-#   chart      = "raw"
-#   version    = "0.2.5"
-#   values = [
-#     <<-EOF
-#     resources:
-#       %{for path in fileset(path.module, "manifests/templates/*yaml")~}
-#       - ${indent(2, templatefile(path, { spacename = local.spacename, space_domain = local.space_domain }))}
-#       %{endfor~}
-#     EOF
-#   ]
-# }
-
-# output "iiyaml" {
-#   value = <<OUTPUT
-# %{for path in fileset(path.module, "manifests/templates/*yaml")~}
-# - ${indent(2, templatefile(path, { spacename = local.spacename, space_domain = local.space_domain }))}
-# %{endfor~}
-# OUTPUT
-# }
